c4baca4d26a5bd6519985417091695812411c039
xielele <1312811411@qq.com>
2024-08-08 09:34:58 +0800

    add Spring Cloud、Config Config+Bus 实现配置的动态刷新

---
+++ micro-service-cloud-config-client-3355/src/main/java/net/biancheng/c/controller/ConfigClientController.java
@@ -0,0 +1,30 @@
+package net.biancheng.c.controller;
+
+/**
+ * @author xiejiale
+ * @date 2024/8/7 15:45
+ **/
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//读取配置中心指定配置文件的内容，并展示到页面
+@RestController
+@RefreshScope //为了让动态（手动）的获取最新的git 配置，在添加 actuator 监控加载 RefreshScope，
+public class ConfigClientController {
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @Value("${config.version}")
+    private String configVersion;
+
+    @GetMapping(value = "/getConfig")
+    public String getConfig() {
+        return "info：" + configInfo + "<br/>version：" + configVersion + "<br/>port：" + serverPort;
+    }
+}
---
+++ micro-service-cloud-config-client-bus-3366/src/main/java/net/biancheng/c/MicroServiceCloudConfigClientBus3366Application.java
@@ -0,0 +1,13 @@
+package net.biancheng.c;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceCloudConfigClientBus3366Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroServiceCloudConfigClientBus3366Application.class, args);
+    }
+
+}
